aac67720fd17965f627e185baa1fb13a5d1d84f5
MapIhs <maptuhul2011@gmail.com>
2024-06-25 21:02:24 +0700

    wip

---
+++ src/content_option.js
@@ -0,0 +1,90 @@
+const logotext = "IHDEV";
+const meta = {
+    title: "Maptuhul Ihsan",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "Maptuhul Ihsan",
+    animated: {
+        first: "I love coding",
+        second: "I love Anime, Comic, Novel",
+        third: "I develop android apps",
+    },
+    description: "I'm a Android Developer, I love coding and I'm always looking for new challenges.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I'm a Android Developer, I love coding and I'm always looking for new challenges. My goal is to make a positive impact on people's lives through the products I create. I'm always looking for opportunities to grow and learn new things.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+];
+
+const skills = [
+    {
+        name: 'Javascript',
+        value: 50,
+    },
+    {
+        name: 'Flutter',
+        value: 70,
+    },
+    {
+        name: 'Go',
+        value: 60,
+    },
+    {
+        name: 'Java',
+        value: 80,
+    }
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "I can Help you with the design of your app, I have experience with Figma",
+},
+{
+    title: "Android Apps",
+    description: "I can help you with the development of your app, I have experience with Java and Flutter.",
+},
+];
+
+const dataportfolio = [
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ihdev@hotmail.com",
+    YOUR_FONE: "+62 89673173894",
+    description: "I'm always looking for new opportunities and challenges. If you have a project that you want to get started, think you need my help with something or just fancy saying hey, then get in touch.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/MapIHS",
+    facebook: "https://facebook.com/Mapihs",
+    linkedin: "https://www.linkedin.com/in/maptuhul-ihsan",
+    twitter: "https://x.com/IhsMap",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
